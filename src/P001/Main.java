9731fb6929a213e05da2ead088154917e82164a7
Cualone <june568@naver.com>
2024-03-25 22:46:16 +0900

    P001

---
+++ src/P001/Main.java
@@ -0,0 +1,21 @@
+package P001;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+        String arr = br.readLine();
+
+        for (int i = 0; i < N; i++) {
+            result += Integer.parseInt(String.valueOf(arr.charAt(i)));
+        }
+
+        System.out.println(result);
+    }
+}
